6d91deef63faa0c6f9c7c44142845f9548786f85
quentin72000 <quentin72000@yahoo.com>
2023-03-13 13:08:59 +0100

    adding a function to auto create the DB, when it don't exist.

--- index.js
+++ index.js
@@ -2,8 +2,20 @@ const {Client, Collection} = require('discord.js')
 require('dotenv').config();
 let pronote = require('./pronote.js')
 let sqlite = require('sqlite3')
+const fs = require("fs")
 
-let db = new sqlite.Database('./data.db', sqlite.OPEN_READWRITE | sqlite.OPEN_CREATE);
+
+let db;
+(async() => {
+    if(!fs.existsSync("data.db")){
+        db = new sqlite.Database('./data.db', sqlite.OPEN_READWRITE | sqlite.OPEN_CREATE);
+        await createDB(db);
+    }
+    else db = new sqlite.Database('./data.db', sqlite.OPEN_READWRITE | sqlite.OPEN_CREATE);
+})()
+
+
+
 
 const client = new Client({
     intents: 32767
@@ -19,10 +31,45 @@ client.db = db
 
 client.pronote = require("./pronote")
 
-client.on("messageCreate", async (message) => {
-    if(message.content.startsWith("!refetch"))pronote.getAllData()
-})
 
 client.login(process.env.BOT_TOKEN)
 require("./handler")(client);
 
+
+
+async function createDB(db){
+
+    db.run(`CREATE TABLE "changementedt" (
+        "date"	TEXT,
+        "prof"	TEXT,
+        "matiere"	TEXT,
+        "horaire"	TEXT
+    )`, (r, err) => handleError(err))
+
+    db.run(`CREATE TABLE "homework" (
+        "id"	TEXT NOT NULL UNIQUE,
+        "matiere"	TEXT,
+        "description"	TEXT,
+        "date_rendue"	TEXT,
+        "date_donne"	TEXT,
+        "fichiers"	TEXT,
+        "fait"	INTEGER,
+        "message_id"	TEXT,
+        PRIMARY KEY("id")
+    )`, (r, err) => handleError(err))
+    
+    db.run(`CREATE TABLE "moyenne" (
+        "matiere"	TEXT,
+        "moyenne"	INTEGER,
+        "moyenne_classe"	INTEGER
+    )`, (r, err) => handleError(err))
+
+    function handleError(err){
+        if(err){
+            console.log("Cannot create DB... Aborting...")
+            console.error(err)
+            process.exit(1)
+        }
+    }
+
+}
